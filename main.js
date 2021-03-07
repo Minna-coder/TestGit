4f82d01777f7e8691cca63f8afe7aa33b84c8b23
Minna Mikkola <minna.mikkola@kotikone.f>
2021-03-07 15:42:20 +0200

    muokattuja juttuja

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
+
+
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
